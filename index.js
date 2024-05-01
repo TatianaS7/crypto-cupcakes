09cd13873e2053a0d76da1f379384831e4521edf
Tatiana <tstwrt7@gmail.com>
2024-05-01 17:11:00 -0400

    Displayed user info when signing in

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-require('dotenv').config('.env');
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const { PORT = 3000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 
-const { User, Cupcake } = require('./db');
-const { auth } = require('express-openid-connect');
+const { User, Cupcake } = require("./db");
+const { auth } = require("express-openid-connect");
 
 // middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
 // follow the docs:
-  // define the config object
-  // attach Auth0 OIDC auth router
-  // create a GET / route handler that sends back Logged in or Logged out
+// define the config object
+// attach Auth0 OIDC auth router
+// create a GET / route handler that sends back Logged in or Logged out
 
-const {
-  AUTH0_SECRET,
-  AUTH0_AUDIENCE,
-  AUTH0_CLIENT_ID,
-  AUTH0_BASE_URL
-  } = process.env;
-  
+const { AUTH0_SECRET, AUTH0_AUDIENCE, AUTH0_CLIENT_ID, AUTH0_BASE_URL } =
+  process.env;
+
 const config = {
   authRequired: true, // this is different from the documentation
   auth0Logout: true,
@@ -40,12 +36,16 @@ const config = {
 
 app.use(auth(config));
 
-
-
-app.get('/cupcakes', async (req, res, next) => {
+app.get("/cupcakes", async (req, res, next) => {
   try {
     const cupcakes = await Cupcake.findAll();
-    res.send(cupcakes);
+    const user = req.oidc.user;
+
+    const greeting = `Welcome, ${user.name}!<br/>
+    Username: ${user.nickname}<br/>
+    <img src=${user.picture}><br/>
+    <button type='button'>See Cupcakes</button>`
+    res.send(greeting);
   } catch (error) {
     console.error(error);
     next(error);
@@ -54,12 +54,11 @@ app.get('/cupcakes', async (req, res, next) => {
 
 // error handling middleware
 app.use((error, req, res, next) => {
-  console.error('SERVER ERROR: ', error);
-  if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message});
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
 });
 
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
-
